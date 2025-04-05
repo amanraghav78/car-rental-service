16bb2453a39f6c804ad0a074dba01a0374ffe86f
Aman <amanraghav682@gmail.com>
2025-04-06 04:15:28 +0530

    improved global exception message

--- src/main/java/com/carrental/car_rental_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/carrental/car_rental_service/exception/GlobalExceptionHandler.java
@@ -48,10 +48,10 @@ public class GlobalExceptionHandler {
 
     public ResponseEntity<Object> buildErrorResponse(String message, HttpStatus status){
         Map<String, Object> body= new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", status.value());
-        body.put("error", status.getReasonPhrase());
-        body.put("message", message);
+        body.put("timestamp: ", LocalDateTime.now());
+        body.put("status: ", status.value());
+        body.put("error: ", status.getReasonPhrase());
+        body.put("message: ", message);
 
         return new ResponseEntity<>(body, status);
     }
